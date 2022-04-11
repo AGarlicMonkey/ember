94aaacd3552472eb1acf77e291f853146a8c85c2
Alex <wolfplayeral@yahoo.co.uk>
2022-04-11 19:48:40 +0100

    Added the tracy profiler to the project

---
+++ source/core/public/ember/core/profiling.hpp
@@ -0,0 +1,9 @@
+#if EMBER_CORE_ENABLE_PROFILING
+    #include <Tracy.hpp>
+
+    #define EMBER_PROFILE_FRAME FrameMark
+    #define EMBER_PROFILE_SCOPE(name) ZoneScopedN(name)
+#else
+    #define EMBER_PROFILE_FRAME
+    #define EMBER_PROFILE_SCOPE(name)
+#endif
