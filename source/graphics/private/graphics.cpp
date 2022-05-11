6b7bf418418d84890c50800861f0375c618b0e3b
Alex <wolfplayeral@yahoo.co.uk>
2022-05-11 07:44:31 +0100

    Log when creating a graphics instance with validation enabled

--- source/graphics/private/graphics.cpp
+++ source/graphics/private/graphics.cpp
@@ -4,8 +4,16 @@
 
 #include <ember/core/log.hpp>
 
+EMBER_LOG_CATEGORY(EmberGraphics)
+
 namespace ember::graphics {
     memory::unique_ptr<instance> create_instance(api api) {
+#if EMBER_GRAPHICS_DEVICE_VALIDATION
+        EMBER_LOG(EmberGraphics, log_level::debug, "Initialising with device validation enabled.");
+#endif
+#if EMBER_GRAPHICS_USAGE_VALIDATION
+        EMBER_LOG(EmberGraphics, log_level::debug, "Initialising with API validation enabled.");
+#endif
         switch(api) {
             case api::vulkan:
                 return create_vulkan_instance();
