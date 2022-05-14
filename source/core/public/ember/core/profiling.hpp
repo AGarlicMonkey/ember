55d551726701fb6407be084e12427158e7403734
Alex <wolfplayeral@yahoo.co.uk>
2022-05-14 19:29:42 +0100

    Use custom named zone markers to avoid collisions

--- source/core/public/ember/core/profiling.hpp
+++ source/core/public/ember/core/profiling.hpp
@@ -1,8 +1,11 @@
 #if EMBER_CORE_ENABLE_PROFILING
     #include <Tracy.hpp>
 
+    #define EMBER_INTERNAL_PROFILER_CONCAT(a, b) EMBER_INTERNAL_PROFILER_CONCAT_2(a, b)
+    #define EMBER_INTERNAL_PROFILER_CONCAT_2(a, b) a##b
+
     #define EMBER_PROFILE_FRAME FrameMark
-    #define EMBER_PROFILE_SCOPE(name) ZoneScopedN(name)
+    #define EMBER_PROFILE_SCOPE(name) ZoneNamedN(EMBER_INTERNAL_PROFILER_CONCAT(_ember_zone_marker_,__LINE__), name, true)
 #else
     #define EMBER_PROFILE_FRAME
     #define EMBER_PROFILE_SCOPE(name)
